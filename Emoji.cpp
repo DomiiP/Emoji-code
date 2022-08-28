ca1b9c587a038b23c06f05c3ccec312cd19981f3
Ankit Malik <68542020+ankitmalik84@users.noreply.github.com>
2022-08-28 08:41:32 +0530

    Add files via upload

---
+++ Emoji.cpp
@@ -0,0 +1,12 @@
+#include<graphics.h>
+#include<iostream>
+using namespace std;
+main(){
+	initwindow(800,800);
+	circle(300,300,200);
+	circle(200,250,40);
+	circle(380,250,40);
+	line(200,400,400,400);
+	cout<<"Ankit first graphic program";
+	getch();
+}
